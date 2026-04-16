maxdimitrov/immich

fix(web): align gallery-viewer viewport zones and skip thumbhash fade for offscreen thumbnails
When thumbnails load outside the viewport (e.g. during fast scrolling),
skip the thumbhash-to-image fade transition to avoid visual flicker for
already-loaded images.

Expands the gallery-viewer sliding window to use the same
INTERSECTION_EXPAND_TOP/BOTTOM tunables as the timeline, enabling
consistent near-viewport preloading of assets across both views.

Adds isInViewport prop to Thumbnail, threaded through AssetLayout, Month,
and Timeline. ViewerAsset gains an isInViewport getter backed by
ViewportProximity.

Also aligns gallery-viewer naming with ViewportProximity terminology:
- isRenderable -> isInOrNearViewport
- isIntersecting -> isInViewport
- onIntersected -> onReachedEnd

Change-Id: Iad30716dc2a45f4883701bbbd1412e106a6a6964

web/src/lib/components/timeline/Month.svelte
@@ -21,6 +21,7 @@
           position: CommonPosition;
           dayGroup: DayGroup;
           groupIndex: number;
+          isInViewport: boolean;
         },
       ]
     >;
@@ -104,8 +105,8 @@
       width={dayGroup.width}
       {customThumbnailLayout}
     >
-      {#snippet thumbnail({ asset, position })}
-        {@render thumbnailWithGroup({ asset, position, dayGroup, groupIndex })}
+      {#snippet thumbnail({ asset, position, isInViewport })}
+        {@render thumbnailWithGroup({ asset, position, dayGroup, groupIndex, isInViewport })}
       {/snippet}
     </AssetLayout>
   </section>

web/src/lib/components/timeline/Timeline.svelte
@@ -670,7 +670,7 @@
             manager={timelineManager}
             onDayGroupSelect={handleGroupSelect}
           >
-            {#snippet thumbnail({ asset, position, dayGroup, groupIndex })}
+            {#snippet thumbnail({ asset, position, dayGroup, groupIndex, isInViewport })}
               {@const isAssetSelectionCandidate = assetInteraction.hasSelectionCandidate(asset.id)}
               {@const isAssetSelected =
                 assetInteraction.hasSelectedAsset(asset.id) || timelineManager.albumAssets.has(asset.id)}
@@ -681,6 +681,7 @@
                 {asset}
                 {albumUsers}
                 {groupIndex}
+                {isInViewport}
                 onClick={(asset) => {
                   if (typeof onThumbnailClick === 'function') {
                     onThumbnailClick(asset, timelineManager, dayGroup, _onClick);

web/src/routes/(user)/search/[[photos=photos]]/[[assetId=id]]/+page.svelte
@@ -293,7 +293,7 @@
       <GalleryViewer
         assets={searchResultAssets}
         assetInteraction={assetMultiSelectManager}
-        onIntersected={loadNextPage}
+        onReachedEnd={loadNextPage}
         showArchiveIcon={true}
         {viewport}
         onReload={onSearchQueryUpdate}

beyond-meet/MyGallery

web/src/lib/components/timeline/Month.svelte
@@ -21,6 +21,7 @@
           position: CommonPosition;
           timelineDay: TimelineDay;
           groupIndex: number;
+          isInViewport: boolean;
         },
       ]
     >;
@@ -105,8 +106,8 @@
       width={timelineDay.width}
       {customThumbnailLayout}
     >
-      {#snippet thumbnail({ asset, position })}
-        {@render thumbnailWithGroup({ asset, position, timelineDay, groupIndex })}
+      {#snippet thumbnail({ asset, position, isInViewport })}
+        {@render thumbnailWithGroup({ asset, position, timelineDay, groupIndex, isInViewport })}
       {/snippet}
     </AssetLayout>
   </section>

web/src/lib/components/timeline/Timeline.svelte
@@ -673,7 +673,7 @@
             manager={timelineManager}
             onTimelineDaySelect={handleGroupSelect}
           >
-            {#snippet thumbnail({ asset, position, timelineDay, groupIndex })}
+            {#snippet thumbnail({ asset, position, timelineDay, groupIndex, isInViewport })}
               {@const isAssetSelectionCandidate = assetInteraction.hasSelectionCandidate(asset.id)}
               {@const isAssetSelected =
                 assetInteraction.hasSelectedAsset(asset.id) || timelineManager.albumAssets.has(asset.id)}
@@ -684,6 +684,7 @@
                 {asset}
                 {albumUsers}
                 {groupIndex}
+                {isInViewport}
                 onClick={(asset) => {
                   if (typeof onThumbnailClick === 'function') {
                     onThumbnailClick(asset, timelineManager, timelineDay, _onClick);

web/src/routes/(user)/search/[[photos=photos]]/[[assetId=id]]/+page.svelte
@@ -292,7 +292,7 @@
       <GalleryViewer
         assets={searchResultAssets}
         assetInteraction={assetMultiSelectManager}
-        onIntersected={loadNextPage}
+        onEndReached={loadNextPage}
         showArchiveIcon={true}
         {viewport}
         onReload={onSearchQueryUpdate}